19f4fe20992f14d080cda6b227ceb5663920921f
Kevin Outta <outaskevin@gmail.com>
2022-06-26 08:18:46 +0300

    modified file

---
+++ printout.js
@@ -0,0 +1,9 @@
+//  javascript code that prints out your name, height, and country on the screen
+
+let myName = "Kevin Outta";
+let myHeight= 178;
+let myCountry = "Kenya";
+
+console.log("my name is",myName);
+console.log("my height is",myHeight);
+console.log("my country is",myCountry);
